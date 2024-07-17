434953c89ba50ad440bb3ad05611e0e837347171
Joy <github.joysaha@gmail.com>
2024-07-17 22:59:53 +0600

    signup database code written

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
 require("dotenv").config();
+const router = require('./routes')
 
-const app = express();
-app.use(cors());
+const app = express()
+app.use(cors())
+app.use("/api", router)
 
 const PORT = 8080 || process.env.PORT;
 
---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+const router = express.Router()
+
+const userSignUpController = require("../controller/userSignUp")
+
+router.post("/signup", router)
+
+
+module.exports = router
