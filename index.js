f1675f88f406f83e58b817c4d8724937b4ff1bf4
Joy <github.joysaha@gmail.com>
2024-07-17 21:31:20 +0600

    database setup done

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-const express = require('express')
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+require("dotenv").config();
 
-const app = express()
-app.use(cors())
+const app = express();
+app.use(cors());
 
-const PORT = 8080 || process.env.PORT
+const PORT = 8080 || process.env.PORT;
 
-app.listen(PORT,()=> {
-    console.log("Server is running")
-})
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("connect to DB")
+    console.log("Server is running");
+  });
+});
